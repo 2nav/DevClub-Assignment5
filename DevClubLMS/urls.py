964c912c2dadc4aa96dffa554ae9538429e03989
2nav <n-raj@outlook.com>
2022-07-31 22:53:22 +0530

    added basic course pages

--- DevClubLMS/urls.py
+++ DevClubLMS/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_views.home),
+    path('course/<int:pk>/',
+         user_views.CourseDetailView.as_view(),
+         name='course-detail')
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from Users.models import Course
+
+
+def is_member(grp, user):
+    return user.groups.filter(name=grp).exists()
+
+
+def home(request):
+    if is_member('Student', request.user):
+        context = {'courses': request.user.courses.all()}
+    elif is_member('Instructor', request.user):
+        context = {'courses': Course.objects.filter(instructor=request.user)}
+    else:
+        context = {'courses': Course.objects.all()}
+
+    return render(request, 'Users/home.html', context)
+
+
+class CourseDetailView(DetailView):
+    model = Course
